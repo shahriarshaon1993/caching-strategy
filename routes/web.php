31e9189f07ce7e6bb0d385fc0413660e0e2dcb47
shahriarshaon1993 <shahriarshaon1993@gmail.com>
2025-09-01 01:49:24 +0600

    first cache

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -15,6 +16,18 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('put-cache', function () {
+        Cache::put('TEST', 'Hello World 2', 60);
+
+        return back();
+    });
+
+    Route::get('get-cache', function () {
+        $data = Cache::get('TEST');
+
+        return response()->json($data);
+    });
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
